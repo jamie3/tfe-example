1e9e7065f88ea40eb998d0c0f2ac6a3028bc1eb7
Jamie Archibald <jamie3@gmail.com>
2019-04-23 16:51:26 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "jamie-tfe-test"
+  name = "jamie-tfe-test-${var.abc}"
   location = "westus2"
 }
+
+variable "abc" {
+  type="string"
+  }
